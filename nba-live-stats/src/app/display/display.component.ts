3b3274e11733509eb24514a55406a83ea943d370
KJ OBrien <kjokjo219@gmail.com>
2025-10-13 21:44:05 -0400

    adds time and time base data

--- nba-live-stats/src/app/display/display.component.ts
+++ nba-live-stats/src/app/display/display.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { TeamComponent } from '../team/team.component';
 import { TeamScoreComponent } from '../team-score/team-score.component';
 import { TeamStatsComponent } from '../team-stats/team-stats.component';
+import { TimeComponent } from '../time/time.component';
 
 @Component({
   selector: 'app-display',
-  imports: [TeamComponent, TeamScoreComponent, TeamStatsComponent],
+  imports: [TeamComponent, TeamScoreComponent, TeamStatsComponent, TimeComponent],
   templateUrl: './display.component.html',
   styleUrl: './display.component.css'
 })
