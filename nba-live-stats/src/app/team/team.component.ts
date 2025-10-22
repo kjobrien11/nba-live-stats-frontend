2e6590ac31ba844f4904eeb23d5d24dc99315e5d
KJ OBrien <kjokjo219@gmail.com>
2025-10-22 19:32:54 -0400

    updates blazers

--- nba-live-stats/src/app/team/team.component.ts
+++ nba-live-stats/src/app/team/team.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Team } from '../interfaces/team';
 
 @Component({
@@ -7,8 +7,17 @@ import { Team } from '../interfaces/team';
   templateUrl: './team.component.html',
   styleUrl: './team.component.css'
 })
-export class TeamComponent {
+export class TeamComponent implements OnChanges{
 
   @Input() team!: Team;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['team'] && this.team) {
+      if(this.team.teamName == "Trail Blazers"){
+        this.team.teamName = "Blazers";
+      }
+    }
+    
+  }
   
 }
